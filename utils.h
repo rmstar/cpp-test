5ec3ae75eb847f0a8f14e232c4e89cbce5c785c6
Prashant Jaikumar <rmstar@gmail.com>
2015-05-31 17:44:29 -0700

    Added a function to return execution time of a lambda with variable number of args.

--- utils.h
+++ utils.h
@@ -28,4 +28,17 @@ namespace utils {
         boost::split(strs, str, boost::is_any_of(delims));
         return strs;
     }
+
+    template <typename F, typename ...Args>
+    auto get_execution_time(F func, Args... args)
+    {
+        typedef std::chrono::high_resolution_clock Clock;
+        typedef std::chrono::duration<double, std::nano> ns;
+        Clock::time_point t0 = Clock::now();
+
+        func(args...);
+        Clock::time_point t1 = Clock::now();
+        ns s0 = t1 - t0;
+        return s0.count();
+    }
 }
