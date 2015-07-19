e554b2098979fbb69f80d93d278cbd48a62f2afb
Prashant Jaikumar <rmstar@gmail.com>
2015-07-18 23:45:59 -0700

    iterate over BITMAP (LSB->MSB) vs printing as string (MSB->LSB)

--- bitmap.cc
+++ bitmap.cc
@@ -1,6 +1,15 @@
 #include <bitset>
 #include <iostream>
 
+template <std::size_t N>
+void iterate_bits(const std::bitset<N> &bs)
+{
+    for(int i = 0; i < bs.size(); ++i) {
+        std::cout << bs[i];
+    }
+    std::cout << "\n";
+}
+
 int main()
 {
     std::bitset<8> bitmap("1001");
@@ -18,5 +27,12 @@ int main()
     std::cout << bitmap << '\n';
     bitmap &= ~std::bitset<8>("1110");
     std::cout << bitmap << '\n';
+    std::cout << "iterate_bits: ";
+    iterate_bits(bitmap);
 
+    std::bitset<4> bs("1110");
+    // prints MSB-> LSB
+    std::cout << bs << "\n";
+    // prints 0111,LSB->MSB
+    iterate_bits(bs);
 }
